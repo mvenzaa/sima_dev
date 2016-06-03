130250db0a21e3dd1f0131a97f1d7966dcbceb30
mvenzaa <mvenzaa@gmail.com>
2016-06-03 19:18:03 +0700

    update tulisan stop

--- app/src/main/java/com/venza/stopnarkoba/MainActivity.java
+++ app/src/main/java/com/venza/stopnarkoba/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity
                 getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.content_frame, fragment);
         fragmentTransaction.commit();
-        setTitle("Stop Narkoba!");
+        setTitle("Stop ");
 
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
